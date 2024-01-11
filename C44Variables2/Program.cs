adff5470755265e012eff7a0a80ae09e1a34abf5
Keanon Born <disposingmoon1@gmail.com>
2024-01-11 12:38:09 -0500

    first

---
+++ C44Variables2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace C44Variables2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string berbName = "Zeke";
+            int berbAge = 1;
+            char berbLastInitial = 'B';
+            bool heScream = true;
+            double weight = 23;
+            decimal wingSpan = 10;
+            Console.WriteLine($"One of my bird's name is {berbName}.");
+            Console.WriteLine($"He is a little over a {berbAge} year old.");
+            Console.WriteLine($"He weighs about {weight} grams.");
+            Console.WriteLine($"His wingspan is about {wingSpan} inches.");
+    }
+}
